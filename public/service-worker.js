63eec99b1d6a68c6574ecb94c5fd78650326324e
ibrahim shittu <ibshittu01@gmail.com>
2022-06-01 02:30:29 +0100

    notif

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,13 +18,13 @@ self.addEventListener('install', function(event) {
 
 // Listen for requests
 self.addEventListener('fetch', (event) => {
-    if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
-        event.waitUntil(
-            this.registration.showNotification("Internet", {
-                body: "internet not working",
-            })
-        )
-    }
+    // if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
+    //     event.waitUntil(
+    //         this.registration.showNotification("Internet", {
+    //             body: "internet not working",
+    //         })
+    //     )
+    // }
 
     event.respondWith(
         caches.match(event.request)
@@ -63,5 +63,26 @@ self.addEventListener('activate', (event) => {
             );
         })
     );
+});
+
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
+  e.waitUntil(
+    self.registration.showNotification('Hello world!', options)
+  );
 });
 
